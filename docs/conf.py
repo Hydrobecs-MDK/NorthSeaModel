b1a422d6be973411a2d27462446983746ec3b9b5
Hydrobecs-MDK <hydrobecs.mdk@gmail.com>
2020-03-31 14:32:58 +0200

    Added default theme.

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -30,7 +31,8 @@ release = 'Private'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc','sphinx_rtd_theme']
+
 #extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -50,7 +52,8 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'yeen'
+#html_theme = 'yeen'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
